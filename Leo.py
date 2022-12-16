980b718f7c53784d506a2c9c07fbbb744d13486d
unknown <leo914yang@gmail.com>
2022-12-16 17:51:01 +0800

    1216

---
+++ Leo.py
@@ -0,0 +1,8 @@
+import os 
+from glob import glob
+
+path = 'python detect.py --weights runs/train/yolov74/weights/best.pt --conf 0.1 --device cpu --source '
+img_path = glob('Aquarium/test/images/*')
+
+for i in img_path:
+	os.system(path + i)
